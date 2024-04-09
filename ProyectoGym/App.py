57059e7559026fec33f554b2a05d4ef0383f56b3
Miguel Angel Escobar Marin <maem1704@gmail.com>
2024-04-08 21:37:37 -0500

    Reestructuración de la organización de carpetas

--- ProyectoGym/App.py
+++ ProyectoGym/App.py
@@ -1,2 +1,11 @@
-from flask import Flask
+from flask import Flask,render_template
 
+app = Flask(__name__)
+
+@app.route("/")
+def Inicio_secion():
+    return render_template('VistaInicioSesion.html')
+@app.route("/register")
+def register():
+    return render_template('Registrarse.html')
+app.run(port=81, debug=True)
