d58da57f5666e9fcb830179409297abcc2a280ba
AlejandroV2204 <jvallarinocruz@gmail.com>
2024-04-29 02:16:45 -0500

    Intento #1 de Validar correctamente el Inicio de Sesion, llevando los datos del usuario al menu

--- ProyectoGym/models/ControlInicioSesion.py
+++ ProyectoGym/models/ControlInicioSesion.py
@@ -1,5 +1,7 @@
 from flask import redirect
 from models.consultas import inicio_sesion
+from flask import session
+from Util.Session import *
 def validar_usuario(email, password):
     # En este ejemplo, se simula la validación con un diccionario    
     # Busca el usuario en la base de datos
@@ -9,3 +11,29 @@ def validar_usuario(email, password):
     else:
         return False
     #return existeUsuario
+    
+def inicio_sesion_prueba(email, password):
+    if 'conectado' in session:
+        print("Ya estaba conectado nea")
+        return dataLoginSesion()
+        #return render_template('public/dashboard/home.html', dataLogin = dataLoginSesion())
+    else:
+        usuario = inicio_sesion(email, password)
+        if usuario:
+            # Crear datos de sesión, para poder acceder a estos datos en otras rutas 
+            session['conectado']        = True
+            session['cedula_usuario']   = usuario[0]
+            session['tipo_usuario']     = usuario[7]
+            session['nombre']           = usuario[1]
+            session['apellido']         = usuario[2]
+            #session['fechaNacimiento'] = usuario[3]
+            session['email']            = usuario[6]
+            session['estado']           = usuario[8]
+
+            msg = "Ha iniciado sesión correctamente."
+            return dataLoginSesion()
+            #return render_template('public/dashboard/home.html', msjAlert = msg, typeAlert=1, dataLogin = dataLoginSesion())                    
+        else:
+            msg = "Usuarios inexistente."
+            return None
+            #return render_template('public/modulo_login/index.html', msjAlert = msg, typeAlert=0)
