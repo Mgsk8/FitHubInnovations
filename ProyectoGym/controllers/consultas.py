9fe02c8aaabba4e7b5b4eef85b961be2c3b02200
unknown <juanchobedoya4@gmail.com>
2024-06-06 01:18:15 -0500

    membresias

--- ProyectoGym/controllers/consultas.py
+++ ProyectoGym/controllers/consultas.py
@@ -194,3 +194,24 @@ def delete(tabla, campo, dato):
             if cursor:
                 cursor.close()
             database.desconectar(conexion)
+
+def registro_membresiag(fecha_inicio, fecha_fin, tipo_membresia, id_cliente):
+    conexion = database.conectar()
+    if conexion:
+        try:
+            cursor = conexion.cursor()
+            sql = "INSERT INTO membresia_cliente (fecha_inicio, fecha_fin, tipo_membresia, id_cliente) VALUES (%s, %s, %s, %s);"
+            cursor.execute(sql, (fecha_inicio, fecha_fin, tipo_membresia, id_cliente))
+            conexion.commit()  # Agregamos el commit para confirmar los cambios
+            print("Usuario registrado exitosamente.")
+            return True
+        except Exception as ex:
+            print(f"Error al crear membresia: {ex}")
+            # NO SE PUDO REGISTRAR USUARIO
+            return False
+        finally:
+            if cursor:
+                cursor.close()
+            database.desconectar(conexion)
+    else:
+        print("Error de conexión a la base de datos.")
--- ProyectoGym/controllers/controlRegistro.py
+++ ProyectoGym/controllers/controlRegistro.py
@@ -1,9 +1,33 @@
+from datetime import datetime, timedelta
 from flask import redirect
-from controllers.consultas import registro 
+from controllers.consultas import registro_membresiag, actualizarFila
 
 
     
+def registro_membresia(fecha_inicio, tipo_membresia, id_cliente):
+    fecha_inicio_dt = datetime.strptime(fecha_inicio, '%Y-%m-%d')
+    fecha_fin_dt = fecha_inicio_dt + timedelta(days=30)
+    fecha_fin = fecha_fin_dt.strftime('%Y-%m-%d')
+    registro_membresiag(fecha_inicio,fecha_fin,tipo_membresia,id_cliente)
 
-def registro_usuario(cedula, nombre, apellido,telefono, fecha_nacimiento, email, tipo_usuario, estado, password):
-    registro(cedula, nombre, apellido, telefono, fecha_nacimiento, email, tipo_usuario, estado, password)
-    
+def editar_membresia(id_membresia,fecha_inicio,tipo_membresia,cedula_cliente):
+    datos = []
+    condicion = f"id_membresia = {id_membresia}"
+    fecha_inicio_dt = datetime.strptime(fecha_inicio, '%Y-%m-%d')
+    fecha_fin_dt = fecha_inicio_dt + timedelta(days=30)
+    fecha_inicio_sql = f"fecha_inicio = '{fecha_inicio_dt.strftime('%Y-%m-%d')}'"
+    fecha_fin_sql = f"fecha_fin = '{fecha_fin_dt.strftime('%Y-%m-%d')}'"
+    tipo_membresia = f"tipo_membresia = {tipo_membresia}"
+    id_cliente = f"id_cliente = '{cedula_cliente}'"
+    datos.append(fecha_inicio_sql)
+    datos.append(fecha_fin_sql)
+    datos.append(tipo_membresia)
+    datos.append(id_cliente)
+    actualizarFila('membresia_cliente',datos,condicion)
+
+def desactivar_membresia(id_membresia):
+    datos = []
+    estado = "estado = 'Inactivo'"
+    datos.append(estado)
+    condicion = f"id_membresia = {id_membresia}" 
+    actualizarFila('membresia_cliente',datos,condicion)
